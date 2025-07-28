5f86d547dc2d7ff7f34c148e26abc9a255393dc7
unkowan <viberus512@gmail.com>
2025-07-28 13:49:37 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,13 @@
 import os
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
-MONGO_URI = os.environ.get("MONGO_URI")
 OWNER_ID = int(os.environ.get("OWNER_ID"))
 RAGHAD_ID = int(os.environ.get("RAGHAD_ID"))
-DB_NAME = os.environ.get("DB_NAME", "shaghaf_db")
-COLLECTION_NAME = os.environ.get("COLLECTION_NAME", "memories")
 
 ALLOWED_USERS = [OWNER_ID, RAGHAD_ID]
 CHANNEL_ID = -1002678532726  # القناة الخاصة
+
+# MongoDB Configuration
+MONGO_URI = os.environ.get("MONGO_URI")
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "shaghaf_db")  # اسم قاعدة البيانات كمتغير مع قيمة افتراضية
+COLLECTION_NAME = os.environ.get("COLLECTION_NAME", "memories")  # اسم الكولكشن كمتغير مع قيمة افتراضية
